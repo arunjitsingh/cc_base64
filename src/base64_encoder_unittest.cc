085570ba84a6e5b1785c7fae05309e941b7f98ab
Arunjit Singh <arunjit@me.com>
2012-02-19 22:01:57 +0530

    Use googletest for unittest

--- src/base64_encoder_unittest.cc
+++ src/base64_encoder_unittest.cc
@@ -6,14 +6,20 @@
 // Test for base64 en/de-coding. Not the actual unittest, but something to test
 // if I'm on the right track.
 
-#include <iostream>
-
 #include <base64/base64_encoder.h>
+#include <gtest/gtest.h>
 
-int main() {
+TEST(Base64EncoderTest, EncodeSingleBlock) {
   std::string input("arunjitsingh");
   ::aj::base64::Base64Encoder enc;
-  enc.Update((void *)input.c_str(), input.size());
-  std::cout << enc.Final() << std::endl;
-  return 0;
+  enc.Update((void *)input.data(), input.size());
+  
+  std::string expected("YXJ1bmppdHNpbmdo");
+
+  EXPECT_EQ(expected, enc.Final());
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
