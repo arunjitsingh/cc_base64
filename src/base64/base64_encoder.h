32fee733e46a83b4cfad637e79518621d4f19154
Arunjit Singh <arunjit@me.com>
2012-02-19 20:03:55 +0530

    Nearly complete implementation of encoder
    I say "nearly complete" because this hasn't been tested yet, but seems
    to compile fine (so far).

--- src/base64/base64_encoder.h
+++ src/base64/base64_encoder.h
@@ -10,6 +10,7 @@
 #define SRC_BASE64_BASE64_ENCODER_H_
 
 #include <string>
+#include <sstream>
 
 #include "base64/base64.h"
 
@@ -19,14 +20,26 @@ namespace base64 {
 // Base64 Encoder class for encoding data into base64.
 class Base64Encoder {
  public:
+  // Initialize the object with whether the encoder should use websafe
+  // characters.
   explicit Base64Encoder(bool use_websafe = false);
+
+  // Update the input with some data.
   bool Update(void *data, size_t size);
+
+  // Finalize the encoding and return the encoded base64 string.
   std::string Final();
+
+  // Get the size of the encoded block so far.
+  size_t size() const {return size_;}
  private:
+  void Encode(bool finalize=false);
+
   bool iswebsafe_;
-  std::string input_;
-  std::string encoded_;
+  byte *input_;
+  uint inputindex_;
   size_t size_;
+  std::stringstream encoded_;
 };
 }
 _END_AJ_NAMESPACE_
--- src/base64_encoder.cc
+++ src/base64_encoder.cc
@@ -12,14 +12,72 @@ namespace base64 {
 
 Base64Encoder::Base64Encoder(bool use_websafe) {
   iswebsafe_ = use_websafe;
+  size_ = 0;
+  inputindex_ = 0;
+  input_ = new byte[kBufferSize];
 }
 
-bool Update(void *data, size_t size) {
-  return false;
+void Base64Encoder::Encode(bool finalize) {
+  if (!finalize && inputindex_ % 3 != 0) {
+    // Unless finalized, don't have enough bytes yet to encode.
+    return;
+  }
+  byte c1, c2, c3, e1, e2, e3, e4;
+  bool h2, h3;
+  uint i = 0;
+  while (i < inputindex_) {
+    c1 = input_[i++];
+    h2 = i < inputindex_;
+    c2 = h2 ? input_[i++] : 0;
+    h3 = i < inputindex_;
+    c3 = h3 ? input_[i++] : 0;
+
+    e1 = c1 >> 2;
+    e2 = ((c1 & 3) << 4) | (c2 >> 4);
+    e3 = ((c2 & 15) << 2) | (c3 >> 6);
+    e4 = c3 & 63;
+
+    if (!h2) {
+      e4 = e3 = 64;
+    } else if (!h3) {
+      e4 = 64;
+    }
+    if (iswebsafe_) {
+      encoded_ << kWebSafeAlphabet[e1] << kWebSafeAlphabet[e2]
+               << kWebSafeAlphabet[e3] << kWebSafeAlphabet[e4];
+    } else {
+      encoded_ << kDefaultAlphabet[e1] << kDefaultAlphabet[e2]
+               << kDefaultAlphabet[e3] << kDefaultAlphabet[e4];
+    }
+  }
+  inputindex_ = 0;
 }
 
-std::string Final() {
-  return std::string("");
+bool Base64Encoder::Update(void *data, size_t size) {
+  byte *bytes = reinterpret_cast<byte *>(data);
+  uint i;
+  // Put bytes in input_, but only till their count is a multiple of 3.
+  for (i = 0; i < size - (size % 3); ++i) {
+    input_[inputindex_++] = bytes[i];
+    if (inputindex_ == kBufferSize) {
+      Encode();
+    }
+  }
+  // Encode input_ only if inputindex_ hasn't been reset.
+  if (inputindex_) {
+    Encode();
+  }
+  // Put remaining bytes in input_. There can only be either 1 or 2 bytes left.
+  for (; i < size; ++i) {
+    input_[inputindex_++] = bytes[i];
+  }
+  return true;
+}
+
+std::string Base64Encoder::Final() {
+  // Encode it one last time for all the remaining bytes.
+  Encode(true);
+  return encoded_.str();
 }
 
 }
