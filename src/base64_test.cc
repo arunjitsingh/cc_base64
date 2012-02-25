52cfc7a2359689c2c5c63a495237fac4291f10e4
Arunjit Singh <arunjit@me.com>
2012-02-25 13:39:56 +0530

    Add Encode-Decode string test

--- src/base64_test.cc
+++ src/base64_test.cc
@@ -9,6 +9,20 @@
 #include <base64/base64_decoder.h>
 #include <gtest/gtest.h>
 
+TEST(Base64Test, EncodeDecodeString) {
+  std::string str("The quick brown fox jumped over the lazy, cross eyed frog");
+
+  ::aj::base64::Base64Encoder enc;
+  enc.Update(reinterpret_cast<const void *>(str.data()), str.size());
+  std::string encoded = enc.Final();
+
+  ::aj::base64::Base64Decoder dec;
+  dec.Update(encoded);
+
+  EXPECT_EQ(str, dec.Final());
+}
+
+
 struct Person {
   std::string name;
   unsigned int age;
@@ -25,7 +39,7 @@ TEST(Base64Test, EncodeDecodeStruct) {
   ::aj::base64::Base64Decoder dec;
   dec.Update(encoded);
   const Person *decoded = reinterpret_cast<const Person *>(dec.Final().data());
-  
+
   EXPECT_EQ(decoded->name, arunjit.name);
   EXPECT_EQ(decoded->age, arunjit.age);
 }
