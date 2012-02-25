b65eeddeaf99f0bc54f813a46bab3ca7b1da0dd7
Arunjit Singh <arunjit@me.com>
2012-02-25 11:33:28 +0530

    Add Encode-Decode struct test

---
+++ src/base64_test.cc
@@ -0,0 +1,36 @@
+// Copyright 2012 Arunjit Singh. All Rights Reserved.
+// Licence: MIT (http://www.opensource.org/licenses/mit-license.php).
+// Redistribution with or without modification must include this notice.
+// Author: Arunjit Singh <arunjit@me.com>
+
+// Test for Base64 encoding and decoding. Uses GoogleTest.
+
+#include <base64/base64_encoder.h>
+#include <base64/base64_decoder.h>
+#include <gtest/gtest.h>
+
+struct Person {
+  std::string name;
+  unsigned int age;
+};
+
+TEST(Base64Test, EncodeDecodeStruct) {
+  Person arunjit;
+  arunjit.name = "Arunjit Singh";
+  arunjit.age = 21;
+  ::aj::base64::Base64Encoder enc;
+  enc.Update(reinterpret_cast<const void *>(&arunjit), sizeof(arunjit));
+  std::string encoded = enc.Final();
+
+  ::aj::base64::Base64Decoder dec;
+  dec.Update(encoded);
+  const Person *decoded = reinterpret_cast<const Person *>(dec.Final().data());
+  
+  EXPECT_EQ(decoded->name, arunjit.name);
+  EXPECT_EQ(decoded->age, arunjit.age);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
